4ff038d7a27f0c8bbe470ecddb316604ea36b303
yuichi <ys.development0601@gmail.com>
2022-01-18 01:52:09 +0900

    itemに番号を振る

--- client/screens/TabThreeScreen.tsx
+++ client/screens/TabThreeScreen.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Dimensions, FlatList, StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
+import AuthTokenContext from '../context/AuthTokenContext';
 
 const data = () => {
   const random = () => Math.random().toString(32).substring(2);
   const list = [];
-  for (let i = 0; i <= 100; ++i) {
-    list.push({ id: random(), name: random() });
+  for (let i = 1; i <= 100; ++i) {
+    list.push({ id: random(), number: i, name: random() });
   }
   return list;
 };
 
-const Item = ({ name }: { name: string }) => (
+const Item = ({ number, name }: { number: number; name: string }) => (
   <View style={styles.item}>
-    <Text style={styles.itemName}>{name}</Text>
+    <Text style={styles.itemName}>{`${number}: ${name}`}</Text>
   </View>
 );
 
 export default function TabThreeScreen() {
+  const { authToken } = useContext(AuthTokenContext);
   return (
     <View style={styles.container}>
-      <FlatList data={data()} keyExtractor={(item) => item.id} renderItem={({ item }) => <Item name={item.name} />} />
+      <FlatList
+        data={data()}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Item number={item.number} name={`${item.name}: ${authToken}`} />}
+      />
     </View>
   );
 }
